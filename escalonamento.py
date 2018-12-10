66cbe7f0e3cf2a93eade7ab868701ad0322ea37a
FernandoNoleto <fernandoleto@hotmail.com>
2018-12-09 22:48:58 -0300

    Trabalho de escalonamento finalizado

--- escalonamento.py
+++ escalonamento.py
@@ -1,28 +1,85 @@
+import threading
+import time
 from random import randint
 import random
 import string
 
-class Palavras(object):
-    
-    def __init__(self):
-        self.palavra = ""
-        self.tamanho = 0
-        self.prioridade = 0
 
-    # def gerar_palavra():
-        # randint(1, 10)
-    def gerar_palavra(self, size, chars=string.ascii_lowercase + string.digits):
+
+class MyThread(threading.Thread):
+    def run(self):
+        print("tam: {} -> {}    INICIADO!".format(len(self.getName()), self.getName()))
+        time.sleep(len(self.getName())/10)
+        print("tam: {} -> {}    FINALIZADO!".format(len(self.getName()), self.getName()))
+        # print("{} finalizado!".format(self.getName()))
+
+
+
+def gerar_palavra(size, chars=string.ascii_lowercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
 
 
+def first_come_first_served(palavras):
+    count = 0
+    for p in palavras:
+        print("{} ->  {}".format(count, p))
+        count +=1
+
+def shortest_job_first(palavras):
+    count = 0
+    palavras.sort(key=len)
+    # palavras.sort(reverse=True)
+    for p in palavras:
+        print("{} - > {}".format(count, p))
+        count +=1
+    
+
+def shortest_remaing_time_next():
+    for x in range(5):
+        # mythread = MyThread(name = "Thread-{}".format(gerar_palavra(randint(1,8))))
+        mythread = MyThread(name = "{}".format(gerar_palavra(randint(8,15))))
+        mythread.start()
+        time.sleep(.1)
 
 
 
 def main():
-    p = Palavras()
+    count = 0
+    palavras = []
     for i in range(5):
-        print(p.gerar_palavra(randint(1,10)))
+        palavras.append(gerar_palavra(randint(1,10)))
+        # print(gerar_palavra(randint(1,10)))
+    
+    
+    # ----------------------------------------palavras geradas---------------------------------------- #
+
+    print("-----------------------------")
+    print("Palavras geradas:")
+    for p in palavras:
+        print("{} ->  {}".format(count, p))
+        count +=1
 
 
-if __name__ == "__main__":
+    # ------------------------------------first_come_first_served------------------------------------- #
+    
+    print("-----------------------------")
+    print("first come first served:")
+    first_come_first_served(palavras)
+    print("-----------------------------")
+    
+    # ---------------------------------------shortest_job_first--------------------------------------- #
+    
+    print("shortest job first:")
+    shortest_job_first(palavras)
+    print("-----------------------------")
+    
+    # ------------------------------------shortest_remaing_time_next---------------------------------- #
+
+    print("shortest remaing time next:")
+    shortest_remaing_time_next()
+    
+
+
+
+if __name__ == '__main__':
     main()
